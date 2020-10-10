2e7b4989f945b34c1a642f469b88f9400735ce7d
mramadan <mramadan@spark-sys.com>
2020-10-10 14:40:18 +0200

    Modifications

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# Landing Page Udacity Nanodegree
+# Landing Page Project
 
-This project requires you to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+## Table of Contents
+
+- [Landing Page Project](#landing-page-project)
+  - [Table of Contents](#table-of-contents)
+  - [Instructions](#instructions)
+  - [ES6](#es6)
+  - [Done](#done)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of
+the Landing Page project. You'll need to convert this project from a static
+project to an interactive one. This will require modifying the HTML and CSS
+files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the
+Udacity Classroom.
+
+
+## ES6
+
+I used several ES6 methods and also Javascript and browser functions which are
+only available in the latest modern browsers, e.g.
+
+Most of the changes have happened in the `js/app.js` file and some changes for
+styling in `styles.css`
+
+## Done
+
+This project has been enhanced with dynamic generated Navigation list (based on
+the sections) in an unordered list as well as scrolling to the section.
--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+// Global variables
+const fragment = document.createDocumentFragment();
+const sectionList = document.querySelectorAll("section");
+const navBarList = document.getElementById("navbar-list");
+const nav = document.getElementById("navbar");
+const navLinks = document.querySelectorAll(".navbar .nav-link");
+// Change navbar on scroll
 const bgNavbar = () => {
   window.addEventListener("scroll", function (e) {
-    const nav = document.getElementById("navbar");
     if (
       document.documentElement.scrollTop ||
       document.body.scrollTop > window.innerHeight
@@ -12,9 +18,49 @@ const bgNavbar = () => {
   });
 };
 
-const makeNavLinksSmooth = () => {
-  const navLinks = document.querySelectorAll(".navbar .nav-link");
+// Create Link Navbar
+const createNavItemHTML = (id, name) => {
+  const itemHTML = `<a class ="nav-link" href="#${id}" data-id="${id}">${name}</a>`;
+  return itemHTML;
+};
 
+// The viewport varies with the device, and will be smaller on a mobile phone than on a computer screen
+const isInViewport = (elem) => {
+  const bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+// Build the navbar
+const buildNavigation = () => {
+  for (let i = 0; i < sectionList.length; i++) {
+    const newMenuItem = document.createElement("li");
+    const sectionName = sectionList[i].getAttribute("data-nav");
+    const sectionId = sectionList[i].getAttribute("id");
+    newMenuItem.innerHTML = createNavItemHTML(sectionId, sectionName);
+    fragment.appendChild(newMenuItem);
+  }
+
+  navBarList.appendChild(fragment);
+};
+
+// Add class 'active' to section when near top of viewport
+const setActiveClass = () => {
+  for (let i = 0; i < sectionList.length; i++) {
+    if (isInViewport(sectionList[i])) {
+      sectionList[i].classList.add("section-active");
+    } else {
+      sectionList[i].classList.remove("section-active");
+    }
+  }
+};
+const makeNavLinksSmooth = () => {
   for (let n in navLinks) {
     if (navLinks.hasOwnProperty(n)) {
       navLinks[n].addEventListener("click", (e) => {
@@ -27,24 +73,11 @@ const makeNavLinksSmooth = () => {
   }
 };
 
-const spyScrolling = () => {
-  const sections = document.querySelectorAll(".bg-hero");
-
-  window.onscroll = () => {
-    const scrollPos =
-      document.documentElement.scrollTop || document.body.scrollTop;
-
-    for (let s in sections)
-      if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos) {
-        const id = sections[s].id;
-        document.querySelector(".active").classList.remove("active");
-        document
-          .querySelector(`a[href*=${id}]`)
-          .parentNode.classList.add("active");
-      }
-  };
-};
-
+// Call function
 bgNavbar();
+createNavItemHTML();
+buildNavigation();
 makeNavLinksSmooth();
-spyScrolling();
+document.addEventListener("scroll", function () {
+  setActiveClass();
+});
