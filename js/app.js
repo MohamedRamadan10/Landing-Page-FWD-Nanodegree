8818af40e4e8c57fa76728be5605c4d57d82fa02
mramadan <mramadan@spark-sys.com>
2020-10-18 07:06:54 +0200

    Resubmit Project

--- js/app.js
+++ js/app.js
@@ -1,83 +1,89 @@
 // Global variables
+const navbar = document.getElementById("navbar");
+const navbarList = document.getElementById("navbar-list");
+const sections = Array.from(document.querySelectorAll("section"));
 const fragment = document.createDocumentFragment();
-const sectionList = document.querySelectorAll("section");
-const navBarList = document.getElementById("navbar-list");
-const nav = document.getElementById("navbar");
-const navLinks = document.querySelectorAll(".navbar .nav-link");
-// Change navbar on scroll
-const bgNavbar = () => {
-  window.addEventListener("scroll", function (e) {
-    if (
-      document.documentElement.scrollTop ||
-      document.body.scrollTop > window.innerHeight
-    ) {
-      nav.classList.add("scroll");
-    } else {
-      nav.classList.remove("scroll");
-    }
-  });
-};
-
-// Create Link Navbar
-const createNavItemHTML = (id, name) => {
-  const itemHTML = `<a class ="nav-link" href="#${id}" data-id="${id}">${name}</a>`;
-  return itemHTML;
-};
-
-// The viewport varies with the device, and will be smaller on a mobile phone than on a computer screen
-const isInViewport = (elem) => {
-  const bounding = elem.getBoundingClientRect();
+const isInViewport = (domElement, threshold = 0) => {
+  const boundaries = domElement.getBoundingClientRect();
   return (
-    bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    bounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth)
+    boundaries.top >= threshold &&
+    boundaries.left >= threshold &&
+    boundaries.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) -
+        threshold &&
+    boundaries.right <=
+      (window.innerWidth || document.documentElement.clientWidth) - threshold
   );
 };
 
-// Build the navbar
-const buildNavigation = () => {
-  for (let i = 0; i < sectionList.length; i++) {
-    const newMenuItem = document.createElement("li");
-    const sectionName = sectionList[i].getAttribute("data-nav");
-    const sectionId = sectionList[i].getAttribute("id");
-    newMenuItem.innerHTML = createNavItemHTML(sectionId, sectionName);
-    fragment.appendChild(newMenuItem);
-  }
-
-  navBarList.appendChild(fragment);
+// Build Nav
+const buildNav = () => {
+  sections.forEach((section, i) => {
+    const navbarItem = document.createElement("li");
+    navbarItem.setAttribute("class", "nav-item");
+    navbarItem.innerHTML = `<a href="#${section.dataset.nav}" class="nav-link">${section.dataset.nav}</a>`;
+    fragment.appendChild(navbarItem);
+  });
 };
 
-// Add class 'active' to section when near top of viewport
-const setActiveClass = () => {
-  for (let i = 0; i < sectionList.length; i++) {
-    if (isInViewport(sectionList[i])) {
-      sectionList[i].classList.add("section-active");
+buildNav();
+navbarList.appendChild(fragment);
+
+// Add background navbar on scroll
+const addBgNavbar = () => {
+  window.onscroll = () => {
+    if (window.scrollY > 300) {
+      navbar.classList.add("scroll");
     } else {
-      sectionList[i].classList.remove("section-active");
+      navbar.classList.remove("scroll");
     }
-  }
-};
-const makeNavLinksSmooth = () => {
-  for (let n in navLinks) {
-    if (navLinks.hasOwnProperty(n)) {
-      navLinks[n].addEventListener("click", (e) => {
-        e.preventDefault();
-        document.querySelector(navLinks[n].hash).scrollIntoView({
-          behavior: "smooth",
-        });
-      });
-    }
-  }
+  };
 };
 
-// Call function
-bgNavbar();
-createNavItemHTML();
-buildNavigation();
-makeNavLinksSmooth();
-document.addEventListener("scroll", function () {
-  setActiveClass();
-});
+addBgNavbar();
+
+// Hide and show navbar on scroll
+window.onscroll = function (e) {
+  const scrollY = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollY <= this.lastScroll) {
+    navbar.classList.add("show");
+    navbar.classList.add("scroll");
+    navbar.classList.remove("hide");
+  } else {
+    navbar.classList.remove("show");
+    navbar.classList.add("hide");
+    navbar.classList.add("scroll");
+  }
+
+  this.lastScroll = scrollY;
+
+  // If user stop scrolling show navbar after 3.5 seconds
+  let time;
+  function checkScroll() {
+    time = setTimeout(function () {
+      navbar.classList.add("scroll");
+      navbar.classList.remove("hide");
+    }, 3500);
+  }
+  checkScroll();
+};
+
+// Add active link
+const activeLinkNavSection = () => {
+  const navbarLinks = document.querySelectorAll("#navbar-list li");
+  const sectionsAll = document.querySelectorAll("section");
+  let index = sections.length;
+  while (--index && window.scrollY + 300 < sections[index].offsetTop) {}
+  navbarLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+  sectionsAll.forEach((link) => {
+    link.classList.remove("section-active");
+  });
+  navbarLinks[index].classList.add("active");
+  sectionsAll[index].classList.add("section-active");
+};
+
+activeLinkNavSection();
+window.addEventListener("scroll", activeLinkNavSection);
